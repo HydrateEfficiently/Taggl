1126cd02a87ae39257d74fe6b42c68a5f0ed705a
Michael Fry-White <michaelfry2002@gmail.com>
2016-04-22 18:45:35 +0100

    Start api controller generation

---
+++ src/Taggl.CodeGeneration/Commands/ApiControllerGenerateComand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.CodeGeneration.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taggl.CodeGeneration.Commands
+{
+    [Alias("api")]
+    public class ApiControllerGenerateComand : GenerateCommand
+    {
+        public ApiControllerGenerateComand(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
---
+++ src/Taggl.CodeGeneration/Generators/ApiController/ApiControllerCommandLineModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.CodeGeneration.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taggl.CodeGeneration.Commands.Models;
+
+namespace Taggl.CodeGeneration.Generators.ApiController
+{
+    public class ApiControllerCommandLineModel : CommandLineModel
+    {
+        [Option(Name = "service", ShortName = "s", Description = "The service to base the controller off")]
+        public string Service { get; set; }
+
+        [Option(Name = "inject", ShortName = "i", Description = "The extra services (comma separated) to inject into the controller")]
+        public string Inject { get; set; }
+    }
+}
