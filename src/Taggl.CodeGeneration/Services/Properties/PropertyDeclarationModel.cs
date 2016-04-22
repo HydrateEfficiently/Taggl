9452fbcad6147eba08080ebfbb638eb0f758bef1
Michael Fry-White <michaelfry2002@gmail.com>
2016-04-22 14:29:17 +0100

    Last commit continued

--- src/Taggl.CodeGeneration/Services/Properties/PropertyDeclarationModel.cs
+++ src/Taggl.CodeGeneration/Services/Properties/PropertyDeclarationModel.cs
@@ -8,9 +8,11 @@ namespace Taggl.CodeGeneration.Services.Properties
     public class PropertyDeclarationModel
     {
         public string AccessModifier { get; internal set; }
-        public string Name { get; internal set; }
-        public string ResolvedTypeName { get; internal set; }
+        public string PropertyName { get; internal set; }
+        public string PropertyTypeName { get; internal set; }
         public string ExtraModifiers { get; internal set; }
         public bool HasExtraModifier { get { return !string.IsNullOrEmpty(ExtraModifiers); } }
+        public IEnumerable<string> Attributes { get; internal set; } = Enumerable.Empty<string>();
+        public bool HasAttribute { get { return Attributes.Count() > 0; } }
     }
 }
