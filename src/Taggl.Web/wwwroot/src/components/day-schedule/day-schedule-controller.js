d922fdd64e472a52032f45627f2a816a9931e865
Michael Fry-White <michaelfry2002@gmail.com>
2016-04-26 16:10:09 +0100

    Declare modal stuff inline

--- src/Taggl.Web/wwwroot/src/components/day-schedule/day-schedule-controller.js
+++ src/Taggl.Web/wwwroot/src/components/day-schedule/day-schedule-controller.js
@@ -30,12 +30,22 @@ export class DayScheduleController extends Injectable {
     addShift() {
         let shifts = this.shifts;
         this.$uibModal.open({
-            controller: CreateShiftController,
-            controllerAs: 'ctrl',
-            templateUrl: `${paths.components}day-schedule/create-shift.html`,
             resolve: {
                 date: this.date
-            }
+            },
+            controller: ($uibModalInstance, date) => ({
+                date,
+                closeModal: (result) => {
+                    $uibModalInstance.close(result);
+                }
+            }),
+            controllerAs: 'ctrl',
+            template: `
+                <tgl-modal-base-directive modal-title="Add Shift">
+                    <div class="mb-md">Add a new shift on {{ ctrl.date | tglMomentCommonDateFormat }}.</div>
+                    <tgl-shift-form date="ctrl.date" on-save="ctrl.closeModal(result)" on-cancel="ctrl.closeModal()"></tgl-shift-form>
+                </tgl-modal-base-directive>
+            `
         }).result.then(shift => {
             if (shift) {
                 shifts.push(shift);
--- src/Taggl.Web/wwwroot/src/components/day-schedule/day-schedule.js
+++ src/Taggl.Web/wwwroot/src/components/day-schedule/day-schedule.js
@@ -4,13 +4,14 @@ import { DayScheduleController } from './day-schedule-controller';
 
 import { commonServices } from './../../common/common-services';
 import { commonFilters } from './../../common/common-filters';
+import { commonDirectives } from './../../common/common-directives';
 import { shiftDetails } from './../shift-details/shift-details';
 import { shiftForm } from './../shift-form/shift-form';
 
 export let daySchedule = componentFactory(
     'daySchedule',
     DayScheduleController,
-    [commonServices, commonFilters, shiftDetails, shiftForm],
+    [commonServices, commonFilters, commonDirectives, shiftDetails, shiftForm],
     {
         timestamp: '@'
     });
